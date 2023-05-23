93a8cc28eba472079dcf962a3d71376090d90524
Olimjon Orifjonov <98266046+olimjon1606@users.noreply.github.com>
2023-05-23 14:49:01 +0200

    js

---
+++ pr1/app.js
@@ -0,0 +1,4 @@
+const argInput = document.querySelector('.arg1_array');
+const result = document.querySelector('.result');
+// const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-calc');
