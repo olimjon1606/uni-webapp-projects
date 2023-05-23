9bf23ec71718cb255a5d617e7f59b6136e041534
Olimjon Orifjonov <98266046+olimjon1606@users.noreply.github.com>
2023-05-23 16:13:21 +0200

    d

--- pr1/app.js
+++ pr1/app.js
@@ -3,8 +3,10 @@ const result = document.querySelector('.result');
 // const todoList = document.querySelector('.todo-list');
 const filterOption = document.querySelector('.filter-calc');
 const calcBtn = document.querySelector('.calc');
+const fillBtn = document.querySelector('.fill');
 
 calcBtn.addEventListener('click', calculate)
+fillBtn.addEventListener('click', fill)
 function calculate() {
     let array = argInput.value.split(' ').join('');
     array = Array.from(array, Number);
@@ -26,13 +28,22 @@ function calculate() {
     }
 
 }
-const n = [5, 2, 3, 4]
+
+function random(){
+    return Math.floor(Math.random()*10)
+}
+function fill(){
+    let n = new Array(10).fill(random())
+    console.log(n)
+
+}
 function sum(array) {
     function f(total, num) {
         return total + num
     }
     return array.reduce(f, 0)
 }
+
 function product(array) {
     function f(total, num) {
         return total * num
@@ -41,6 +52,4 @@ function product(array) {
 }
 const max = array => array.sort(f = (a, b) => a - b)[array.length - 1]
 const min = array => array.sort(f = (b, a) => a - b)[array.length - 1]
-console.log(max(n))
-console.log(min(n))
 
