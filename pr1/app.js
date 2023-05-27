fab156317ae3480f1105b8bd8d20adc658162258
Olimjon Orifjonov <98266046+olimjon1606@users.noreply.github.com>
2023-05-27 23:46:00 +0200

    Update app.js

--- pr1/app.js
+++ pr1/app.js
@@ -12,8 +12,47 @@ const saveBtn = document.querySelector('.save');
 calcBtn.addEventListener('click', calculate)
 fillBtn.addEventListener('click', fill)
 saveBtn.addEventListener('click', save)
+function calculate() {
+    let array = argInput.value.split(' ');
+    array = Array.from(array, Number);
 
+    switch (filterOption.value) {
+        case "sum":
+            result.value = sum(array);
+            break;
+        case "product":
+            result.value = product(array);
+            break;
+        case "maximum":
+            result.value = max(array)
+            break;
+        case "minimum":
+            result.value = min(array)
+            break;
+    }
 
+}
+function random() {
+    return Math.floor(Math.random() * 10)
+}
+function fill() {
+    let n = new Array(10).fill()
+    n = n.map(f = (i) => i = random())
+    argInput.value = n.join(' ')
+}
+function sum(array) {
+    function f(total, num) {
+        return total + num
+    }
+    return array.reduce(f, 0)
+}
+
+function product(array) {
+    function f(total, num) {
+        return total * num
+    }
+    return array.reduce(f, 1)
+}
 const max = array => array.sort(f = (a, b) => a - b)[array.length - 1]
 const min = array => array.sort(f = (b, a) => a - b)[array.length - 1]
 
